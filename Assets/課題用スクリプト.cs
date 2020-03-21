3da14d814c277655cfefaf5a3702b09e88dd59c7
西村和人 <kazuzon.0418@gmail.com>
2020-03-21 12:16:16 +0900

    Script Test

---
+++ Assets/課題用スクリプト.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class 課題用スクリプト : MonoBehaviour {
+    
+	// Use this for initialization
+	void Start () {
+        int[] array = { Random.Range(1, 10), Random.Range(1, 10), Random.Range(1, 10), Random.Range(1, 10), Random.Range(1, 10) };
+        for (int i = 0; i < 5; i++)
+        {
+            Debug.Log(array[i]);
+        }
+        for (int i = 4; i>= 0; i-=1)
+        {
+            Debug.Log(array[i]);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
